b63b89e6fca1330bfa6d59e00d9726ccd20b90f8
Joanna Wang <zhao.wang@duke.edu>
2018-12-03 14:53:41 -0500

    deleting

--- data/prepareDUMLData.R
+++ data/prepareDUMLData.R
@@ -4,7 +4,6 @@ library(fields)
 library(lme4)
 library(ggplot2)
 
-#testing 
 # Read in data
 # positional data about the RV Kahuna
 # kahuna <- read_csv('../../../presentations/2018/2018-12-03_DUML-RepResearch/data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv') 
